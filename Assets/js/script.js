bc951e4cb7a6fe503358dba3a1f03db8128baf05
Aurora Duncan <aurorabrynn@gmail.com>
2021-03-27 14:45:44 -0700

    bleeeeeeeeeeeh

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,52 +3,76 @@ var start = document.querySelector("#start");
 var timeNum = document.querySelector("#timeNum");
 var timer = document.querySelector("#timer");
 var main = document.querySelector("#main");
+var answerSection = document.querySelector("#answerSection");
 var questionPlace = document.querySelector("#questionPlace");
 var firstAnswer = document.querySelector("#firstAnswer");
 var secondAnswer = document.querySelector("#secondAnswer");
 var thirdAnswer = document.querySelector("#thirdAnswer");
 var fourthAnswer = document.querySelector("#fourthAnswer");
+var score = "";
 
-var questions = [
-    {
-        question: "What does it mean when cats wag their tail?",
-        answer1: "They're happy",
-        answer2: "They're angry", //correct
-        answer3: "They're hunting",
-        answer4: "They're scared"
-    },
-    {
-        question: "What were dachshunds bred to do?",
-        answer1: "To herd",
-        answer2: "To be cute",
-        answer3: "To fight badgers", //correct
-        answer4: "To guard"
-    },
-    {
-        question: "What does a slow blink mean in cat language?",
-        answer1: "I trust you", //correct
-        answer2: "I hate you",
-        answer3: "You look tasty",
-        answer4: "Rub my tummy"
-    },
-    {
-        question: "Dogs' sense of smell is __ than ours.",
-        answer1: "Slightly worse",
-        answer2: "Slightly better",
-        answer3: "15 times better",
-        answer4: "40 times better" //correct
-    }
-];
+var question1 = {
+    question: "What does it mean when cats wag their tail?",
+    answer1: "They're happy",
+    correctAnswer: "They're angry", //correct
+    answer3: "They're hunting",
+    answer4: "They're scared"
+}
+var question2 = {
+    question: "What were dachshunds bred to do?",
+    answer1: "To herd",
+    answer2: "To be cute",
+    correctAnswer: "To fight badgers", //correct
+    answer4: "To guard"
+}
+var question3 = {
+    question: "What does a slow blink mean in cat language?",
+    correctAnswer: "\"I trust you\"", //correct
+    answer2: "\"I hate you\"",
+    answer3: "\"You look tasty\"",
+    answer4: "\"Rub my tummy\""
+}
+var question4 = {
+    question: "Dogs' sense of smell is __ than ours.",
+    answer1: "Slightly worse",
+    answer2: "Slightly better",
+    answer3: "15 times better",
+    correctAnswer: "40 times better" //correct
+}
 
 //game starts when I click button
 function startGame() {
     isGameRunning = true;
     startTimer();
+
+    //presented question
+    questionPlace.textContent = question1.question;
+    firstAnswer.textContent = question1.answer1;
+    secondAnswer.textContent = question1.correctAnswer;
+    thirdAnswer.textContent = question1.answer3;
+    fourthAnswer.textContent = question1.answer4;
+
+    question1.correctAnswer.addEventListener("click", endGame()) /*{
+        questionPlace.textContent = question2.question;
+        firstAnswer.textContent = question2.answer1;
+        secondAnswer.textContent = question2.answer2;
+        thirdAnswer.textContent = question2.correctAnswer;
+        fourthAnswer.textContent = question2.answer4;
+    })*/
 }
+
+/*function next() {
+    questionPlace.textContent = question2.question;
+    firstAnswer.textContent = question2.answer1;
+    secondAnswer.textContent = question2.answer2;
+    thirdAnswer.textContent = question2.correctAnswer;
+    fourthAnswer.textContent = question2.answer4;
+}*/
+
 //timer starts
 function startTimer() {
     clearInterval(timer);
-    timeLeft = 75;
+    timeLeft = 5;
     timeNum.textContent = timeLeft;
     timer = setInterval(function () {
         timeLeft--;
@@ -57,15 +81,20 @@ function startTimer() {
         if (timeLeft < 1) {
             isGameRunning = false;
             clearInterval(timer);
+            endGame();
             //localStorage.setItem('losses', losses);
         }
     }, 1000)
 }
-//presented question
 //if i answer right, presented another question
 //if i answer wrong, time is subtracted from clock
 //when all questions answered, game is over
+
 //presented score (timer = score)
+function endGame() {
+    questionPlace.innerHTML = "Score: " + timeLeft;
+    answerSection.innerHTML = "";
+}
 //can save initals and score
 
 
