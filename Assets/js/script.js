6b11c15c21b71ae092596094c9539fb88ddfd4f3
Aurora Duncan <aurorabrynn@gmail.com>
2021-03-29 18:55:33 -0700

    fixed most questions problems, added score and initials

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,22 @@
-var isGameRunning = false;
+var i = 0;
+var timeLeft = 0;
 var start = document.querySelector("#start");
 var timeNum = document.querySelector("#timeNum");
 var timer = document.querySelector("#timer");
 var main = document.querySelector("#main");
 var answerSection = document.querySelector("#answerSection");
 var questionPlace = document.querySelector("#questionPlace");
-var firstAnswer = document.querySelector("#firstAnswer");
-var secondAnswer = document.querySelector("#secondAnswer");
-var thirdAnswer = document.querySelector("#thirdAnswer");
-var fourthAnswer = document.querySelector("#fourthAnswer");
+var firstAnswer = document.querySelector("#answer1");
+var secondAnswer = document.querySelector("#answer2");
+var thirdAnswer = document.querySelector("#answer3");
+var fourthAnswer = document.querySelector("#answer4");
+var answerBtns = document.querySelectorAll(".answer");
+var gameEl = document.querySelector("#game");
+var messageEl = document.querySelector("#message");
+var rightOrWrong = document.querySelector("#rightOrWrong");
+var initials = document.querySelector("#initials");
+var save = document.querySelector("#save");
+var isGameRunning = false;
 var score = "";
 var questions = [
     {
@@ -17,7 +25,7 @@ var questions = [
         answer2: "They're angry", //correct
         answer3: "They're hunting",
         answer4: "They're scared",
-        correctAnswer: "answer2"
+        correctAnswer: "They're angry"
     },
     {
         question: "What were dachshunds bred to do?",
@@ -25,7 +33,7 @@ var questions = [
         answer2: "To be cute",
         answer3: "To fight badgers", //correct
         answer4: "To guard",
-        correctAnswer: "answer3"
+        correctAnswer: "To fight badgers"
     },
     {
         question: "What does a slow blink mean in cat language?",
@@ -33,7 +41,7 @@ var questions = [
         answer2: "\"I hate you\"",
         answer3: "\"You look tasty\"",
         answer4: "\"Rub my tummy\"",
-        correctAnswer: "answer1"
+        correctAnswer: "\"I trust you\""
     },
     {
         question: "Dogs' sense of smell is __ than ours.",
@@ -41,27 +49,52 @@ var questions = [
         answer2: "Slightly better",
         answer3: "15 times better",
         answer4: "40 times better", //correct
-        correctAnswer: "answer4"
+        correctAnswer: "40 times better"
     }
-]
+];
 
 //game starts when I click button
-function startGame(questions) {
+function startGame() {
     isGameRunning = true;
     startTimer();
-    var output = [];
+    present();
+}
 
+function present() {
     //presented question
-    for (var i = 0; i < questions.length; i++) {
-        output.push(
-            questionPlace.textContent = questions[i].question,
-            firstAnswer.textContent = questions[i].answer1,
-            secondAnswer.textContent = questions[i].answer2,
-            thirdAnswer.textContent = questions[i].answer3,
-            fourthAnswer.textContent = questions[i].answer4
-        );
+    questionPlace.textContent = questions[i].question
+    firstAnswer.textContent = questions[i].answer1
+    secondAnswer.textContent = questions[i].answer2
+    thirdAnswer.textContent = questions[i].answer3
+    fourthAnswer.textContent = questions[i].answer4
+    //when all questions answered, game is over
+}
+
+function toggleElementDisplay(element) {
+    if (window.getComputedStyle(element).display === "none") {
+        element.style.display = "block"
+    } else {
+        element.style.display = "none"
     }
 }
+
+answerBtns.forEach(function (ansBtn) {
+    ansBtn.addEventListener("click", function (event) {
+        var userGuess = event.target.textContent
+        //if i answer right, presented another question
+        if (userGuess === questions[i].correctAnswer) {
+            rightOrWrong.innerHTML = "Right!"
+            i++
+            present();
+            endGame();
+            //if i answer wrong, time is subtracted from clock
+        } else {
+            rightOrWrong.innerHTML = "Wrong!"
+            timeLeft -= 15;
+        }
+    })
+})
+//when all questions answered, game is over
 
 //timer starts
 function startTimer() {
@@ -81,18 +114,26 @@ function startTimer() {
     }, 1000)
 }
 
-//if i answer right, presented another question
-//if i answer wrong, time is subtracted from clock
-//when all questions answered, game is over
-
 //presented score (timer = score)
 function endGame() {
     isGameRunning = false;
     clearInterval(timer);
     questionPlace.innerHTML = "Score: " + timeLeft;
-    answerSection.innerHTML = "";
+    answerSection.innerHTML = toggleElementDisplay(initials);
+    rightOrWrong.innerHTML = "";
+    toggleElementDisplay(save);
+
+    //can save initals and score
+    save.addEventListener("click", function () {
+        localStorage.setItem('score', timeLeft);
+        localStorage.setItem('user', initials.value);
+    })
 }
-//can save initals and score
 
 
-start.addEventListener("click", startGame(questions))
+start.addEventListener("click", function () {
+    toggleElementDisplay(messageEl)
+    toggleElementDisplay(gameEl)
+    startGame();
+})
+present();
